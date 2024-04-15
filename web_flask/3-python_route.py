1d3fa8bbe454940ca8dcbe7fd7f137eb3dcef58c
Kamalusta <kamalusta@gmail.com>
2024-04-16 01:00:57 +0400

    4

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""doc"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False )
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    new_text = text.replace('_', ' ')
+    return f"C {new_text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    new_text = text.replace('_', ' ')
+    return f"Python {new_text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
