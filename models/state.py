2ff39c631b3cabec1b125d0560de91a13150f70f
Kamal Hüseynli <99289704+Kamalusta@users.noreply.github.com>
2024-04-19 09:48:58 +0400

    Update state.py

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-from models.engine.file_storage import FileStorage
+from models import storage
 
 
 class State(BaseModel, Base):
